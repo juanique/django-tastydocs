4fc16078c1fbf55d42925ac3b8a4dbc5c7cc9869
juanique <juanique@ubuntu-prometheus.(none)>
2012-09-10 15:22:12 -0300

    removed unused models

--- tastydocs/models.py
+++ tastydocs/models.py
@@ -1,11 +1,1 @@
-from django.db import models
-import jsonfield
 
-
-class ExampleData(models.Model):
-
-    resource = models.CharField(max_length=32)
-    method = models.CharField(max_length=32)
-    format = models.CharField(max_length=32)
-    created = models.DateField()
-    data = jsonfield.JSONField()
