775d724f5b941c55327e366d5fd5e05e015c9202
juanique <juanique@ubuntu-prometheus.(none)>
2012-09-12 13:15:57 -0300

    fixed test db management for doc examples

--- tastydocs/views.py
+++ tastydocs/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render_to_response
 from django.db import connection
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
-from chocolate.rest import TastyFactory
 from django.http import HttpResponse
+from django.conf import settings
+
+from chocolate.rest import TastyFactory
 from tastypie.serializers import Serializer
 
 try:
@@ -14,8 +16,8 @@ except ImportError:
 
 class test_db(object):
 
-    def __init__(self, verbosity=0):
-        self.db_name = ""
+    def __init__(self, verbosity=1):
+        self.old_db_config = dict(settings.DATABASES["default"])
         self.verbosity = verbosity
 
     def __enter__(self):
@@ -27,7 +29,8 @@ class test_db(object):
         connection.creation.create_test_db(self.verbosity)
 
     def __exit__(self, type, value, traceback):
-        connection.creation.destroy_test_db(self.db_name, self.verbosity)
+        connection.creation.destroy_test_db(self.old_db_config['NAME'], self.verbosity)
+        settings.DATABASES['default'] = dict(self.old_db_config)
 
 
 def doc(request, api):
--- test_project/blog/tests.py
+++ test_project/blog/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.test.client import Client
-import json
 
 
 class BaseTestCase(TestCase):
@@ -15,19 +14,33 @@ class BaseTestCase(TestCase):
 
 class DocsTestCase(BaseTestCase):
 
+    def get_example_url(self, resource_name):
+        kwargs = {
+            'resource_name': resource_name,
+        }
+
+        return reverse("tastydocs.views.example_data", kwargs=kwargs)
+
     def test_example_data(self):
         "Example data can be requested using a json webservice"
 
         client = Client()
-        kwargs = {
-            'resource_name': 'comment',
-        }
 
-        url = reverse("tastydocs.views.example_data", kwargs=kwargs)
-
+        url = self.get_example_url("comment")
         response = client.get(url)
         content_type = response._headers['content-type'][1]
 
         self.assertEquals(200, response.status_code)
         self.assertTrue(content_type.startswith("application/json"))
         self.assertTrue("/api/v1/post/1/" in response.content, response.content)
+
+    def test_docs_twise(self):
+        "BUG REPRO: Cannot request example data twice."
+
+        client = Client()
+
+        url = self.get_example_url("comment")
+        response = client.get(url)
+        response = client.get(url)  # yes, twice
+
+        self.assertEquals(200, response.status_code)
